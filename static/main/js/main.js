55f4b1a5cced59931c8c387b0302b62e8a1724d4
equqe <oaouzc@gmail.com>
2024-07-17 17:00:26 +0300

    initial

---
+++ static/main/js/main.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // инициализация слайдера
+    const slider = new ItcSimpleSlider('.itcss', {
+        autoplay: true,
+        interval: 10000,
+    });
+  });
